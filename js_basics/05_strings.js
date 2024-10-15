7158259c25cf65089b37b6b912d600e435a6f664
Nik2390 <nikhil.nikhil.borkar@gmail.com>
2024-10-15 14:45:29 +0000

    string done

---
+++ js_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name = "nikhil"
+const myAge = 34
+
+//console.log(name + myAge + " At 2024 year");
+
+//console.log(`My name is ${name} and my age is ${myAge} at 2024 year`);
+
+const gameName = new String("mougaly")
+//console.log(gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const newStringOne = "  Raju  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://nikhil.com/nikhil%20borkar"
+console.log(url.replace('%20', '-'))
